f77dadaf0d7a375e3782d44738ce43ef30641207
Alexey Stepanov <st3p-by-step@ya.ru>
2023-03-26 21:16:42 +0300

    Logging is added to the Process class

--- src/main/java/com/agents/models/Process.java
+++ src/main/java/com/agents/models/Process.java
@@ -7,6 +7,8 @@ import com.agents.MessageType;
 
 import java.net.Socket;
 import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static java.lang.Thread.sleep;
 
@@ -14,12 +16,13 @@ public class Process extends Client {
     private final String orderName;
     private final Dish dish;
     private String cookerName;
+    private final Logger logger;
 
     public Process(Socket socket, String clientName, String orderName, Dish dish) {
         super(socket, clientName);
-
         this.orderName = orderName;
         this.dish = dish;
+        this.logger = Logger.getLogger(Process.class.getName());
     }
 
     @Override
@@ -43,11 +46,10 @@ public class Process extends Client {
         this.cookerName = cookerName;
         try {
             Message dishRespond = new Message(cookerName, this.clientName, MessageType.DishRequestRespond, dish.toJson());
-
             sendMessage(dishRespond);
+            logger.log(Level.INFO, this.clientName + ": Sent dish request to " + cookerName);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            // TODO
+            logger.log(Level.SEVERE, this.clientName + ": Failed to send dish request to " + cookerName, e);
         }
     }
 
@@ -56,9 +58,9 @@ public class Process extends Client {
         try {
             long time = dish.getTime();
             sleep(time);
+            logger.log(Level.INFO, this.clientName + ": Finished cooking dish " + dish.getName() + " for order " + orderName);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            // TODO
+            logger.log(Level.SEVERE, this.clientName + ": Failed to cook dish " + dish.getName() + " for order " + orderName, e);
         }
         ended();
     }
@@ -67,17 +69,19 @@ public class Process extends Client {
         try {
             Message processEndNotification = new Message(cookerName, this.clientName, MessageType.ProcessRespond);
             sendMessage(processEndNotification);
+            logger.log(Level.INFO, this.clientName + ": Sent process end notification to " + cookerName);
 
             for (String instrument : dish.getInstruments()) {
                 processEndNotification.setDestination(instrument);
                 sendMessage(processEndNotification);
+                logger.log(Level.INFO, this.clientName + ": Sent process end notification to " + instrument);
             }
 
             processEndNotification.setDestination(orderName);
             sendMessage(processEndNotification);
+            logger.log(Level.INFO, this.clientName + ": Sent process end notification to order " + orderName);
         } catch (Exception e) {
-            System.out.println(e.getMessage());
-            // TODO
+            logger.log(Level.SEVERE, this.clientName + ": Failed to send process end notification", e);
         }
     }
 }
